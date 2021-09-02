d8fe97b59f2f15246779e982a6c8453a6fca984e
KathyVelito <85207829+KathyVelito@users.noreply.github.com>
2021-09-01 21:11:00 -0500

    chore: Primer dia

---
+++ dia1/02-impresion-pantalla.py
@@ -0,0 +1,14 @@
+mes = "Agosto"
+
+print("el mes es:",mes)
+
+dia = 16
+
+print("el dia", dia, "del mes de ", mes)
+
+alumnos = 23
+grupo = 9
+
+print("los alumnos son {} y son del grupo {} y todos estan atentos".format(alumnos,grupo))
+
+print(f"los alumnos son {alumnos}")
